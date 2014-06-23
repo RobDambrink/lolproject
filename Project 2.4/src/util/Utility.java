4d44c29e60fe0f7a693a6fb2564689f9ed2ac972
martijndoornik <m.doornik94@gmail.com>
2014-06-23 12:34:45 +0200

    ertfyhk

---
+++ Project 2.4/src/util/Utility.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+public class Utility {
+
+	
+	public static void returnJSON(HttpServletResponse response, JSONObject json) throws IOException {
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.print(json);
+		out.flush();
+	}
+}
