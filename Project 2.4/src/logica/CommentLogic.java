5551ecf3509aaa0a60955627dd0baab015185061
rrvk <rvankammen@live.nl>
2014-06-24 16:14:28 +0200

    comments ook goed gemaakt dat je ze kunt opvragen

--- Project 2.4/src/logica/CommentLogic.java
+++ Project 2.4/src/logica/CommentLogic.java
@@ -1,6 +1,11 @@
 package logica;
 
+import java.util.HashMap;
+import java.util.List;
+
+import net.sf.json.JSONObject;
 import databaseConnection.Hibernate;
+import exeption.PageNotExist;
 import mappingHibernate.BuildComment;
 import mappingHibernate.Comments;
 import mappingHibernate.MasteryComment;
@@ -12,11 +17,14 @@ public class CommentLogic {
 	public static final String MASTERYBULD ="mastery";
 	
 	private Hibernate hib;
-	public CommentLogic(Hibernate hib){
+	public CommentLogic(Hibernate hib) throws PageNotExist{
 		this.hib=hib;
 		addComment(ITEMBULD, "bla bla test item", 1L, 1L);
 		addComment(RUNESBULD, "bla bla test rune", 1L, 2L);
 		addComment(MASTERYBULD, "bla bla test mastery", 1L, 3L);
+		System.out.println(getComments(1l,ITEMBULD));
+		System.out.println(getComments(2l,RUNESBULD));
+		System.out.println(getComments(3l,MASTERYBULD));
 	}
 	
 	/**
@@ -25,34 +33,97 @@ public class CommentLogic {
 	 * @param accountId the accountId
 	 * @param Id this is the id of the page you are commenting on
 	 * @param comment the comment
+	 * @throws PageNotExist 
 	 */
-	public void addComment(String kind, String comment, Long accountId, Long id){
+	public void addComment(String kind, String comment, Long accountId, Long id) throws PageNotExist{
 		 switch (kind) {
          case ITEMBULD:	
-        	 Comments com = addCommentOnly(comment, accountId);
-        	 BuildComment build = new BuildComment();
-        	 build.setBuildid(id);
-        	 build.setCommentid(com.getId());
-        	 hib.addToDatabase(build);
-        	 // TODO CONTROLEREN OF HET BESTAAD?
+        	 ItemBuldLogica itemBuld = new ItemBuldLogica(hib);
+        	 if(itemBuld.getBuild(id)!=null){
+	        	 Comments com = addCommentOnly(comment, accountId);
+	        	 BuildComment build = new BuildComment();
+	        	 build.setBuildid(id);
+	        	 build.setCommentid(com.getId());
+	        	 hib.addToDatabase(build);
+        	 }
+        	 else
+        		 throw new PageNotExist("This itemBuldPage Does not exist");
         	 break;
-         case RUNESBULD:	
-        	 com = addCommentOnly(comment, accountId);
-        	 RuneComment rune = new RuneComment();
-        	 rune.setCommentid(com.getId());
-        	 rune.setRuneid(id);
-        	 hib.addToDatabase(rune);
-        	 // TODO CONTROLEREN OF HET BESTAAD?
+         case RUNESBULD:
+        	 RunePageLogic runePage = new RunePageLogic(hib);
+        	 if(runePage.getRunePage(id)!=null){
+        		 Comments com = addCommentOnly(comment, accountId);
+	        	 RuneComment rune = new RuneComment();
+	        	 rune.setCommentid(com.getId());
+	        	 rune.setRuneid(id);
+	        	 hib.addToDatabase(rune);
+        	 }
+        	 else
+        		 throw new PageNotExist("This runePage does not exist");
         	 break;
          case MASTERYBULD:	
-        	 com = addCommentOnly(comment, accountId);
-        	 MasteryComment mas = new MasteryComment();
-        	 mas.setCommentid(com.getId());
-        	 mas.setMasteryid(id);
-        	 hib.addToDatabase(mas);
-        	 // TODO CONTROLEREN OF HET BESTAAD?
+        	 MasteriesPageLogic masteryPage = new MasteriesPageLogic(hib);
+        	 if(masteryPage.getMasteryPage(id)!=null){
+        		 Comments com = addCommentOnly(comment, accountId);
+	        	 MasteryComment mas = new MasteryComment();
+	        	 mas.setCommentid(com.getId());
+	        	 mas.setMasteryid(id);
+	        	 hib.addToDatabase(mas);
+        	 }
+        	 else
+        		 throw new PageNotExist("This masteryPage does not exist");
         	 break;
 		 }
+	}
+	
+	public JSONObject getComments(Long id, String name){
+		HashMap<Long, Comments> hm = new HashMap<Long, Comments>();
+		switch (name) {
+	        case ITEMBULD:	
+	        	List<?> list = hib.getDataFromDatabase("FROM BuildComment WHERE buildid =" + id + "");
+	       	 	if (list!=null && !list.isEmpty()){
+	       	 		for (int i = 0; i < list.size(); i++) {
+	       	 			BuildComment comment = (BuildComment) list.get(i);
+	       	 			hm.put(comment.getCommentid(), getCommentById(comment.getCommentid()));
+	       	 		}
+	       	 	}
+	       	 	else
+	       	 		return null;
+       	 	break;
+	        case RUNESBULD:	
+	        	list = hib.getDataFromDatabase("FROM RuneComment WHERE runeid =" + id + "");
+	       	 	if (list!=null && !list.isEmpty()){
+	       	 		for (int i = 0; i < list.size(); i++) {
+	       	 			RuneComment comment = (RuneComment) list.get(i);
+	       	 			hm.put(comment.getCommentid(), getCommentById(comment.getCommentid()));
+	       	 		}
+	       	 	}
+	       	 	else
+	       	 		return null;
+       	 	break;
+	        case MASTERYBULD:	
+	        	list = hib.getDataFromDatabase("FROM MasteryComment WHERE masteryid =" + id + "");
+	       	 	if (list!=null && !list.isEmpty()){
+	       	 		for (int i = 0; i < list.size(); i++) {
+	       	 			MasteryComment comment = (MasteryComment) list.get(i);
+	       	 			hm.put(comment.getCommentid(), getCommentById(comment.getCommentid()));
+	       	 		}
+	       	 	}
+	       	 	else
+	       	 		return null;
+       	 	break;
+		}
+		JSONObject obj = new JSONObject();
+		obj.putAll(hm);
+		return obj;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private Comments getCommentById(Long id){
+		List list = hib.getDataFromDatabase("FROM Comments WHERE id = " + id + "");
+		if (list!=null && !list.isEmpty())
+			return (Comments)list.get(0);
+		return null;
 	}
 	
 	private Comments addCommentOnly(String comment, Long accountId){
