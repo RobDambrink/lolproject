da5f8ea9680d59b79e66aa8cc80297420e075509
martijndoornik <m.doornik94@gmail.com>
2014-06-24 20:37:45 +0200

    Matches by Summoner ID

---
+++ Project 2.4/src/control/Matches/GetMatchesFromSummoner.java
@@ -0,0 +1,56 @@
+package control.Matches;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.riot.ResponseException;
+
+import databaseConnection.CouchDB;
+import databaseConnection.Hibernate;
+import logica.SummonerLogica;
+import util.JSONUtility;
+
+/**
+ * Servlet implementation class GetMatchesFromSummoner
+ */
+@WebServlet(description = "Get all matches from a summoner", urlPatterns = { "/Summoner/GetMatchesBySummonerId" })
+public class GetMatchesFromSummoner extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetMatchesFromSummoner() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			long id = Long.parseLong(request.getParameter("id"));
+			SummonerLogica sl = new SummonerLogica(new Hibernate(), new CouchDB());
+			sl.getMatchHistory(id);
+			JSONUtility.sendJSON(response, sl.getAllMatchesFromDatabase(id));
+		} catch(NumberFormatException e) {
+			JSONUtility.sendError(response, "Invalid format.");
+		} catch (ResponseException e) {
+			JSONUtility.sendError(response, "Something went wrong.");
+		}
+	}
+
+}
